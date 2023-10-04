2439e6e3cce0be9f7088601398715c4da72b3f86
dhzdhd <ragas5432100@gmail.com>
2023-10-04 21:27:06 +0530

    Add sir dep for rust scss

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 #![allow(non_snake_case, unused)]
 use dioxus::prelude::*;
+use sir::{css, global_css, AppStyle};
 
 #[cfg(target_arch = "wasm32")]
 fn main() {
-    dioxus_web::launch(app);
+    dioxus_web::launch(App);
 }
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    dioxus_desktop::launch(app);
+    dioxus_desktop::launch(App);
 }
 
-fn app(cx: Scope) -> Element {
+fn App(cx: Scope) -> Element {
     let mut count = use_state(cx, || 0);
+
+    let container = css!(
+        "
+        background-color: red;
+        color: blue;
+    "
+    );
+
     cx.render(rsx! {
-        h1 { "High-Five counter: {count}" }
+        AppStyle {}
+        h1 { class: {container}, "High-Fivse counter: {count}" }
+        h1 { "hi" }
         button { onclick: move |_| count += 1, "Up high!" }
         button { onclick: move |_| count -= 1, "Down low!" }
     })
